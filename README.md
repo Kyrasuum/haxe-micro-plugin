be6b2398fb3648b5adcfb1fd1b29230418f0402c
Phillip Lorentz <plorentz7@gmail.com>
2021-12-06 19:04:23 -0500

    added yaml

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # haxe-micro-plugin
-Auto haxe linter for micro editor
+Haxe syntax for Micro Editor 
+
+Install by placing the syntax file into your syntax directory:
+
+curl https://raw.githubusercontent.com/Kyrasuum/haxe-micro-plugin/main/haxe.yaml > ~/.config/micro/syntax/haxe.yaml
---
+++ test.hx
@@ -0,0 +1,14 @@
+/**
+    Multi-line comments for documentation.
+**/
+class Main {
+    static public function main():Void {
+        // Single line comment
+        trace("Hello World");
+    }
+}
+abstract AbstractInt(Int) {
+    inline public function new(i:Int) {
+        this = i;
+    }
+}
